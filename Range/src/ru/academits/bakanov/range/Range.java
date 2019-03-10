07aff23f8623de1b2d7999f54361542dc9bc4b39
Snbnv <snbnv@yandex.ru>
2019-03-10 13:21:12 +0700

    1 коммит Range*

--- Range/src/ru/academits/bakanov/range/Range.java
+++ Range/src/ru/academits/bakanov/range/Range.java
@@ -9,6 +9,9 @@ public class Range {
         this.to = to;
     }
 
+    public Range() {
+    }
+
     public double getFrom() {
         return from;
     }
@@ -31,6 +34,49 @@ public class Range {
 
     public boolean isInside(double x) {
         return (x >= from && x <= to);
+    }
+
+    public Range getIntersection(Range r1, Range r2) {
+        if (r1.to < r2.from || r2.to < r1.from) {
+            return null;
+        } else {
+            Range rangeIntersection = new Range();
+            rangeIntersection.from = r1.from <= r2.from ? r2.from : r1.from;
+            rangeIntersection.to = r1.to <= r2.to ? r1.to : r2.to;
+            return rangeIntersection;
+        }
+    }
+
+    public Range[] getUnion(Range r1, Range r2) {
+        if (r1.to < r2.from || r2.to < r1.from) {
+            return new Range[]{r1, r2};
+            //System.out.printf("интервал объединения - (%.2f - %.2f,  %.2f - %.2f)", r1.from, r1.to, r2.from, r2.to);
+            //System.out.println();
+        } else {
+            Range rangeNew = new Range();
+            rangeNew.from = r1.from <= r2.from ? r1.from : r2.from;
+            rangeNew.to = r1.to <= r2.to ? r2.to : r1.to;
+            return new Range[]{rangeNew};
+            //System.out.printf("интервал объединения (%.2f-%.2f)", this.from, this.to);
+            //System.out.println();
+        }
+    }
+
+    public void getDifference(Range r1, Range r2) {
+        if (r1.to < r2.from || r2.to < r1.from) {
+            this.from = r1.from;
+            this.to = r1.to;
+            System.out.printf("интервал разности (%.2f-%.2f)", this.from, this.to);
+        } else if (r1.from < r2.from && r1.to > r2.to) {
+            r1.to = r2.from;
+            r2.from = r2.to;
+            r2.to = r1.to;
+            Range[] rangeNew = {r1, r2};
+            System.out.printf("интервал разности - (%.2f - %.2f, %.2f - %.2f)", r1.from, r1.to, r2.from, r2.to);
+            System.out.println();
+        } else {
+            System.out.printf("интервал разности (%.2f-%.2f)", r1.from, r2.from);
+        }
     }
 }
 
--- Range/src/ru/academits/bakanov/userange/UseRange.java
+++ Range/src/ru/academits/bakanov/userange/UseRange.java
@@ -1,19 +1,36 @@
 package ru.academits.bakanov.userange;
-
 import ru.academits.bakanov.range.Range;
+import java.util.Arrays;
 
 class UseRange {
     public static void main(String[] args) {
         Range range = new Range(1, 10);
+        Range range2 = new Range(5, 15);
+        Range rangeNew = new Range();
         double x = 5.2;
 
         System.out.println("Длинна диапозона = " + range.getLength());
 
         if (range.isInside(x)) {
-            System.out.println("Число лежит в диапозоне");
+            System.out.printf("Число %.1f лежит в диапозоне (%.1f-%.1f)", x, range.getFrom(), range.getTo());
+            System.out.println();
         } else {
-            System.out.println("Число не лежит в диапозоне");
+            System.out.printf("Число %.1f  не лежит в диапозоне (%.1f-%.1f)", x, range.getFrom(), range.getTo());
+            System.out.println();
         }
+
+        Range rangeIntersection = rangeNew.getIntersection(range, range2);
+        if (rangeIntersection == null) {
+            System.out.println("интервалы не пересекаются");
+        } else {
+            System.out.printf("интервал пересечения - (%.1f-%.1f)", rangeIntersection.getFrom(), rangeIntersection.getTo());
+            System.out.println();
+        }
+
+        Range[] rangeUnion = rangeNew.getUnion(range, range2);
+        System.out.println("интервал объединения  (" + Arrays.toString(rangeUnion) + ")");
+
+        rangeNew.getDifference(range, range2);
     }
 }
 
