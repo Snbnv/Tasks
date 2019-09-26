b47ad4279f9c4a47049c447fcfffbfd97569c658
snbnv <snbnv@yandex.ru>
2019-09-26 11:47:50 +0700

    Converter commit 1

--- Temperature/src/MainConverter.java
+++ Temperature/src/MainConverter.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.util.Objects;
 
 public class MainConverter extends JFrame {
     private static String celsius = "\u2103";
@@ -17,7 +14,9 @@ public class MainConverter extends JFrame {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         SwingUtilities.invokeLater(() -> {
+
             JFrame converter = new JFrame("Temperature converter");
             converter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             converter.setResizable(true);
@@ -26,6 +25,7 @@ public class MainConverter extends JFrame {
             converter.setIconImage(icon);
             converter.setSize(500, 400);
             converter.setLocationRelativeTo(null);
+
 
             JPanel panel = new JPanel(new GridBagLayout());
 
@@ -40,101 +40,85 @@ public class MainConverter extends JFrame {
             constraints.ipady = 10;
 
             constraints.gridy = 1;
-            JTextField enterTemp = new JTextField(10);
-            enterTemp.setHorizontalAlignment(JTextField.CENTER);
-            panel.add(enterTemp, constraints);
+            JTextField enterData = new JTextField(10);
+            enterData.setHorizontalAlignment(JTextField.CENTER);
+            panel.add(enterData, constraints);
+
+            constraints.gridx = 1;
+            JTextField outputDataCK = new JTextField(10);
+            outputDataCK.setHorizontalAlignment(JTextField.CENTER);
+            outputDataCK.setEnabled(false);
+            panel.add(outputDataCK, constraints);
+
+            constraints.gridx = 2;
+            JTextField outputDataCF = new JTextField(10);
+            outputDataCF.setHorizontalAlignment(JTextField.CENTER);
+            outputDataCF.setEnabled(false);
+            panel.add(outputDataCF, constraints);
 
             constraints.gridx = 0;
             constraints.gridy = 2;
+            constraints.ipady = 5;
             String[] box = new String[]{celsius, kelvin, fahrenheit};
-            JComboBox chooserCKF = new JComboBox(box);
+            JComboBox<String> chooserCKF = new JComboBox<>(box);
             panel.add(chooserCKF, constraints);
 
-            constraints.gridx = 1;
-            constraints.gridy = 1;
-            JTextField outputCKTemp = new JTextField(10);
-            outputCKTemp.setHorizontalAlignment(JTextField.CENTER);
-            outputCKTemp.setEnabled(false);
-            panel.add(outputCKTemp, constraints);
-
-            constraints.gridx = 0;
             constraints.gridy = 0;
-            constraints.ipady = 5;
+            constraints.insets = new Insets(5, 10, 0, 0);
             JLabel labelEnterTemp = new JLabel("Enter temperature");
             panel.add(labelEnterTemp, constraints);
 
             constraints.gridx = 1;
             constraints.gridy = 2;
-            constraints.ipady = 5;
             JLabel labelCK = new JLabel(kelvin);
             panel.add(labelCK, constraints);
 
             constraints.gridx = 2;
-            constraints.gridy = 2;
-            constraints.ipady = 5;
             JLabel labelCF = new JLabel(fahrenheit);
             panel.add(labelCF, constraints);
-
-            constraints.gridx = 2;
-            constraints.gridy = 1;
-            constraints.ipady = 10;
-            JTextField outputCFTemp = new JTextField(10);
-            outputCFTemp.setHorizontalAlignment(JTextField.CENTER);
-            outputCFTemp.setEnabled(false);
-            panel.add(outputCFTemp, constraints);
 
             constraints.gridx = 1;
             constraints.gridy = 4;
             constraints.ipadx = 30;
             constraints.ipady = 20;
-            constraints.insets = new Insets(50, 20, 0, 0);
+            constraints.insets = new Insets(50, 0, 0, 0);
             JButton button = new JButton("Convert");
             panel.add(button, constraints);
+
             button.addActionListener(e -> {
-                if (chooserCKF.getSelectedItem().equals(celsius)){
-                    try{
-                        double tempCK = Double.parseDouble(enterTemp.getText()) + 273.15;
-                        double tempCF = Double.parseDouble(enterTemp.getText()) * 9 / 5 + 32;
+                try {
+                    double dataCK;
+                    double dataCF;
+
+                    if (Objects.requireNonNull(chooserCKF.getSelectedItem()).equals(celsius)) {
+                        dataCK = Double.parseDouble(enterData.getText()) + 273.15;
+                        dataCF = Double.parseDouble(enterData.getText()) * 9 / 5 + 32;
 
                         labelCK.setText(kelvin);
                         labelCF.setText(fahrenheit);
 
-                        outputCKTemp.setText(Double.toString(tempCK));
-                        outputCFTemp.setText(Double.toString(tempCF));
-                    }catch(Exception x){
-                        JOptionPane.showMessageDialog(converter,"incorrect enter data");
-                    }
-                }
-                if (chooserCKF.getSelectedItem().equals(kelvin)) {
-                    try {
-                        double tempCK = Double.parseDouble(enterTemp.getText()) - 273.15;
-                        double tempCF = (Double.parseDouble(enterTemp.getText()) - 273.15) * 9 / 5 + 32;
+                    } else if (chooserCKF.getSelectedItem().equals(kelvin)) {
+                        dataCK = Double.parseDouble(enterData.getText()) - 273.15;
+                        dataCF = (Double.parseDouble(enterData.getText()) - 273.15) * 9 / 5 + 32;
 
                         labelCK.setText(celsius);
                         labelCF.setText(fahrenheit);
 
-                        outputCKTemp.setText(Double.toString(tempCK));
-                        outputCFTemp.setText(Double.toString(tempCF));
-                    } catch (Exception x) {
-                        JOptionPane.showMessageDialog(converter, "incorrect enter data");
-                    }
-                }
-                if (chooserCKF.getSelectedItem().equals(fahrenheit)) {
-                    try {
-                        double tempCK = (Double.parseDouble(enterTemp.getText()) - 32) * 5 / 9 + 273.15;
-                        double tempCF = (Double.parseDouble(enterTemp.getText()) - 32) * 5 / 9;
+                    } else {
+                        dataCK = (Double.parseDouble(enterData.getText()) - 32) * 5 / 9 + 273.15;
+                        dataCF = (Double.parseDouble(enterData.getText()) - 32) * 5 / 9;
 
                         labelCK.setText(kelvin);
                         labelCF.setText(celsius);
-
-                        outputCKTemp.setText(Double.toString(tempCK));
-                        outputCFTemp.setText(Double.toString(tempCF));
-                    } catch (Exception x) {
-                        JOptionPane.showMessageDialog(converter, "incorrect enter data");
                     }
+
+                    outputDataCK.setText(Double.toString(dataCK));
+                    outputDataCF.setText(Double.toString(dataCF));
+
+                } catch (Exception x) {
+                    JOptionPane.showMessageDialog(converter, "incorrect enter data");
                 }
             });
-
             converter.setContentPane(panel);
         });
     }
